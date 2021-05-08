578d9213cb9cef59a60f740a587b12a9117d76f7
skhomm <i.kwibus@gmail.com>
2021-05-09 00:18:44 +0300

    Add more expressions, small fixes

--- get_diag.py
+++ get_diag.py
@@ -2,72 +2,78 @@ import subprocess
 import re
 from datetime import datetime
 
-FOLDER_NAME = "wifi_diag_logs"
+FOLDER_NAME = 'wifi_diag_logs'
 
 TASKS = {
-    "ifconfig": {
-        "command": "ifconfig en0",
-        "filename": "ifconfig",
-        "expressions": [
-            r"ether \S+",
-            r"inet6 .+",
-            r"inet .+"
-        # "expressions": [
-        #     r"ether (?P<ethernet>\S+)",
-        #     r"inet6 (?P<ipv6>.+)",
-        #     r"inet (?P<ipv4>.+)"
+    'log_show': {
+        'command': 'log show --info --debug --last 1m',
+        'filename': 'log_show',
+        'expressions': []
+    },
+    'ifconfig': {
+        'command': 'ifconfig en0',
+        'filename': 'ifconfig',
+        'expressions': [
+            r'ether \S+',
+            r'inet6 .+',
+            r'inet .+'
         ]
     },
-    "system_profiler": {
-        "command": "system_profiler SPAirPortDataType",
-        "filename": "system_profiler",
-        "expressions": []
+    'system_profiler': {
+        'command': 'system_profiler SPAirPortDataType',
+        'filename': 'system_profiler',
+        'expressions': [
+            r'Card Type: .+',
+            r'Firmware Version: .+',
+            r'Supported Channels: .+',
+        ]
     },
-    "log_show": {
-        "command": "log show --info --debug --last 1m",
-        "filename": "log_show",
-        "expressions": []
+    'airport': {
+        'command': 'airport -Is',
+        'filename': 'airport',
+        'expressions': [
+            r'\S+ \S+ -\d+ .+'
+        ]
     },
-    "airport": {
-        "command": "airport -Is",
-        "filename": "airport",
-        "expressions": []
-    },
-    "wdutil": {
-        "command": "wdutil info",
-        "filename": "wdutil",
-        "expressions": []
+    'wdutil': {
+        'command': 'wdutil info',
+        'filename': 'wdutil',
+        'expressions': []
     },
 }
 
 
 def get_diagnostics(task, subfolder_name='.'):
-    now = datetime.now().strftime("%y%m%d_%H%M%S")
-    filename = f'{task["filename"]}_{now}.txt'
+    now = datetime.now().strftime('%y%m%d_%H%M%S')
+    filename = f"{task['filename']}_{now}.txt"
 
-    print(task["command"])
+    print(f"\nExecuting: <{task['command']}>")
     process = subprocess.run(
-        task["command"].split(),
+        task['command'].split(),
         stdout=subprocess.PIPE,
         universal_newlines=True
     )
 
+    output = process.stdout
+
     with open(f'{subfolder_name}/{filename}', 'w') as f:
-        for line in process.stdout:
+        for line in output:
             f.write(line)
 
-        for expression in task["expressions"]:
-            search_result = re.findall(expression, process.stdout)
+        search_results = []
+        for expression in task['expressions']:
+            search_results.extend(re.findall(expression, output))
+        for search_result in search_results:
             print(search_result)
 
-    return process.stdout
+    return search_results
 
 
 def main():
     # Create folder <wifi_diag_logs> if it doesn't exist
     # Create subfolder <wifi_diag_date_time> each time program runs
-    now = datetime.now().strftime("%y%m%d_%H%M%S")
-    subfolder_name = (f"{FOLDER_NAME}/wifi_diag_{now}")
+    now = datetime.now().strftime('%y%m%d_%H%M%S')
+    subfolder_name = (f'{FOLDER_NAME}/wifi_diag_{now}')
     subprocess.run(f'mkdir {FOLDER_NAME}'.split())
     subprocess.run(f'mkdir {subfolder_name}'.split())
 
